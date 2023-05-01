350c98a44bf103cbefc05cd2a170e0bbb23ce093
Chris <123729087+chrish121@users.noreply.github.com>
2023-05-01 11:24:14 +0800

    Add functions

---
+++ javascript.js
@@ -0,0 +1,44 @@
+function operate(firstNum, calcOp, secondNum) {
+    if (calcOp = `+`) {
+        return (add(firstNum, secondNum));
+    }
+    if (calcOp = `-`) {
+        return (subtract(firstNum, secondNum));
+    }
+    if (calcOp = `*`) {
+        return (multiply(firstNum, secondNum));
+    }
+    if (calcOp = `/`) {
+        return (divide(firstNum, secondNum));
+    } else {
+        return("Try again.");
+    }
+}
+
+function add(...numbers) {
+    const sum = numbers.reduce((total, num) => {
+        return total + num;
+    }, 0);
+    return sum;
+}
+
+function subtract(...numbers) {
+    const minus = numbers.reduce((total, num) => {
+        return total - num;
+    });
+    return minus;
+}
+
+function multiply(...numbers) {
+    const times = numbers.reduce((total, num) => {
+        return total * num;
+    });
+    return times;
+}
+
+function divide(...numbers) {
+    const division = numbers.reduce((total, num) => {
+        return total/num;
+    });
+    return division;
+}
