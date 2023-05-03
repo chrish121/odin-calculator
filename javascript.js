15fce3a97b27fcfd08b072ba7d12a1f4cb3bb3bd
Chris <123729087+chrish121@users.noreply.github.com>
2023-05-03 10:32:55 +0800

    Add operate() function to equalButton event listener

--- javascript.js
+++ javascript.js
@@ -1,14 +1,14 @@
 function operate(firstNum, calcOp, secondNum) {
-    if (calcOp = `+`) {
+    if (calcOp === `plus`) {
         return (add(firstNum, secondNum));
     }
-    if (calcOp = `-`) {
+    if (calcOp === `minus`) {
         return (subtract(firstNum, secondNum));
     }
-    if (calcOp = `*`) {
+    if (calcOp === `multiply`) {
         return (multiply(firstNum, secondNum));
     }
-    if (calcOp = `/`) {
+    if (calcOp === `divide`) {
         return (divide(firstNum, secondNum));
     } else {
         return("Try again.");
@@ -53,23 +53,22 @@ displayContainer.appendChild(calcDisplay);
 
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
-        calcDisplay.textContent += `${button.className}`;
+        calcDisplay.textContent += `${button.textContent}`;
         calcDisplay.setAttribute = ("style", "font-family: 'Courier New', Courier, monospace, serif; color: black; font-size: 65px; display: flex; flex-shrink: -3")
     });
 })
 
 numberButtons.forEach((numButton) => {
     numButton.addEventListener("click", () => {
-        numClicked = `${numButton.className}`;
+        numClicked = parseInt(`${numButton.textContent}`);
     })
 })
 
 operatorButtons.forEach((opButton) => {
     opButton.addEventListener("click", () => {
         firstNumber = numClicked;
-        firstOperation = `${opButton.className}`;
-        console.log(firstNumber);
-        console.log(firstOperation);
+        firstOperation = `${opButton.textContent}`;
+        firstOperationClass = `${opButton.className}`;
     })
 })
 
@@ -77,6 +76,10 @@ const equalButton = document.querySelector("#equal");
 
 equalButton.addEventListener("click", () => {
     secondNumber = numClicked;
+    console.log(firstNumber);
+    console.log(firstOperation);
     console.log(secondNumber);
     console.log(calcDisplay.textContent);
+    let answer = operate(firstNumber, firstOperationClass, secondNumber);
+    console.log(answer);
 })
