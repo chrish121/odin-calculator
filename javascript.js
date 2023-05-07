4639db3c8b31947fba9312e7147cb0a34a13953f
Chris <123729087+chrish121@users.noreply.github.com>
2023-05-07 15:02:57 +0800

    Change background color at button click, remove change in font color

--- javascript.js
+++ javascript.js
@@ -62,13 +62,13 @@ let zeroAnswer = 0;
 let equalPress = 0;
 let backButton = 0;
 
+
+
 buttons.forEach((button) => {
     button.addEventListener("click", (event) => {
-        event.target.style.backgroundColor = "rgb(80, 80, 80)";
-        event.target.style.color = "white";
+        event.target.style.backgroundColor = "rgb(203, 203, 203)";
         setTimeout(() => {
             event.target.style.backgroundColor = "";
-            event.target.style.color = "";
         }, 250)
     })
 })
